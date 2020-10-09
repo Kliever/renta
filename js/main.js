dea18723be64d4925658858383eca9946913a066
Kliever <64734174+Kliever@users.noreply.github.com>
2020-10-09 13:45:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-$(function(){$(".slider").slick({nextArrow:'<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',fade:true,autoplay:true,responsive:[{breakpoint:441,settings:{arrows:false,fade:false}}]})});
+$(function () {
+  $('.slider').slick({
+    nextArrow:'<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+    prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
+    fade: true,
+    responsive:[
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false,
+          fade: false 
+        }
+      }
+    ]
+  });
+});
+
+
+
+  
